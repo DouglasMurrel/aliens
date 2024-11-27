2bac123a31468b5bd46b1042bed223e7c938abee
kolchin <murrel@yandex.ru>
2024-11-27 22:48:45 +0300

    vk login widget

--- src/Security/VKAuthenticator.php
+++ src/Security/VKAuthenticator.php
@@ -94,19 +94,4 @@ class VKAuthenticator extends OAuth2Authenticator implements AuthenticatorInterf
             Response::HTTP_TEMPORARY_REDIRECT
         );
     }
-    
-    private function randomStr(
-        int $length = 64,
-        string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    ): string {
-        if ($length < 1) {
-            throw new \RangeException("Length must be a positive integer");
-        }
-        $pieces = [];
-        $max = mb_strlen($keyspace, '8bit') - 1;
-        for ($i = 0; $i < $length; ++$i) {
-            $pieces []= $keyspace[random_int(0, $max)];
-        }
-        return implode('', $pieces);
-    }
 }
